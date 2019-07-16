84fe7768322eaa9b96507e01c420665847cddc7a
Maurice <mauricium.maurice@yahoo.com>
2019-07-16 05:15:24 -0700

    (Modified Controllers)

--- server/middleware/checkAdmin.js
+++ server/middleware/checkAdmin.js
@@ -1,6 +1,6 @@
 
 const checkAdmin = (req, res, next) => {
-    (req.decoded.isAdmin) ? next() : res.status(200).send({ Message: 'You are not permitted' });
+    (req.decoded.is_admin) ? next() : res.status(200).send({ Message: 'You are not permitted' });
   }
   
   export default checkAdmin;
