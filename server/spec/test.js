fa4b2258a7a6c2bd2e591d500032b9f93deba71f
Maurice <mauricium.maurice@yahoo.com>
2019-07-16 12:10:10 -0700

    (Modified the test file)

--- server/spec/test.js
+++ server/spec/test.js
@@ -183,7 +183,7 @@ describe('All test cases for WarFarer Application', () => {
           });
       });
       it('Should throw error when a user wants to acces this option', (done) => {
-        request.patch('/api/v1/trips/44')
+        request.patch('/api/v1/trips/2')
           .set('x-access-token', myToken)
           .send({
             status: 'active'
@@ -226,37 +226,68 @@ describe('All test cases for WarFarer Application', () => {
   //         });
   //     });
   //   });
-  //   describe('test case for retriving all loan in the QuickCredit', () => {
-  //     it('should return `401` status code with `res.body` failed messages', (done) => {
-  //       request.get('/api/v1/loans')
-  //         .set('x-access-token', wrongToken)
-  //         .expect(401)
-  //         .end((err, res) => {
-  //           expect(res.body.status).to.equal('Failed');
-  //           expect(res.body.message).to.equal('Authentication failed. Token is either invalid or expired');
-  //           done();
-  //         });
-  //     });
-  //     it('should get a one loan return `201` status code with `res.body` success messages', (done) => {
-  //       request.get('/api/v1/loans/1')
-  //         .set('x-access-token', myToken)
-  //         .expect(201)
-  //         .end((err, res) => {
-  //           expect(res.body).to.have.property('status');
-  //           expect(res.body).to.have.property('message');
-  //           done();
-  //         });
-  //     });
-  //     it('should get loan id return `422 status code with `res.body` success messages', (done) => {
-  //       request.get('/api/v1/loans/22')
-  //         .set('x-access-token', wrongToken)
-  //         .expect(422)
-  //         .end((err, res) => {
-  //           expect(res.body).to.have.property('status');
-  //           expect(res.body).to.have.property('message');
-  //           done();
-  //         });
-  //     });
+    describe('test case for retriving trips', () => {
+      it('should return `401` status code with `res.body` failed messages', (done) => {
+        request.get('/api/v1/trips')
+          .set('x-access-token', wrongToken)
+          .expect(401)
+          .end((err, res) => {
+            expect(res.body).to.have.property('status');
+            expect(res.body).to.have.property('message');
+            done();
+          });
+      });
+      it('should get not bookings', (done) => {
+        request.get('/api/v1/trips/1')
+          .set('x-access-token', wrongToken)
+          .expect(401)
+          .end((err, res) => {
+            expect(res.body).to.have.property('status');
+            expect(res.body).to.have.property('message');
+            done();
+          });
+      });
+      it('should get not bookings', (done) => {
+        request.get('/api/v1/trips/1')
+          .set('x-access-token', wrongToken)
+          .expect(401)
+          .end((err, res) => {
+            expect(res.body).to.have.property('status');
+            expect(res.body).to.have.property('message');
+            done();
+          });
+      });
+      // describe('test case for retriving trips', () => {
+      //   it('should return `201` status code with `res.body` success messages', (done) => {
+      //     request.get('/api/v1/trips')
+      //       .set('x-access-token', myToken)
+      //       console.log(myToken, "==============oooooo")
+      //       .expect(201)
+      //       .end((err, res) => {
+      //         console.log(res.body);
+      //         done();
+      //       });
+      //   });
+      // it('should get a one loan return `201` status code with `res.body` success messages', (done) => {
+      //   request.get('/api/v1/loans/1')
+      //     .set('x-access-token', myToken)
+      //     .expect(201)
+      //     .end((err, res) => {
+      //       expect(res.body).to.have.property('status');
+      //       expect(res.body).to.have.property('message');
+      //       done();
+      //     });
+      // });
+      it('should get not bookings', (done) => {
+        request.get('/api/v1/bookings')
+          .set('x-access-token', wrongToken)
+          .expect(401)
+          .end((err, res) => {
+            expect(res.body).to.have.property('status');
+            expect(res.body).to.have.property('message');
+            done();
+          });
+      });
   //     it('should get repaid loans return `201 status code with `res.body` success messages', (done) => {
   //       request.get('/api/v1/loans/repaid')
   //         .set('x-access-token', myToken)
@@ -310,23 +341,56 @@ describe('All test cases for WarFarer Application', () => {
   //         });
   //     });
   //   });
-  //   describe('All test case loan Application', () => {
-  //     it('Should create a new loan application and return a `201`', (done) => {
-  //       const applyLoan = {
-  //         firstName: 'xanda',
-  //         lastName: 'cage',
-  //         email: 'cage@yahoo.com',
-  //         tenor: '3 month',
-  //         amount: '55,000'
-  //       };
-  //       request.post('/api/v1/loans')
-  //         .send(applyLoan)
-  //         .expect(201)
-  //         .end((err, res) => {
-  //           console.log(res.body, '===================');
-  //           done();
+  it('should get not bus', (done) => {
+    request.get('/api/v1/bus')
+      .set('x-access-token', wrongToken)
+      .expect(401)
+      .end((err, res) => {
+        expect(res.body).to.have.property('status');
+        expect(res.body).to.have.property('message');
+        done();
+      });
+  });
+    // describe('All test case loan Application', () => {
+    //   it('Should create a new bus and return a `201`', (done) => {
+    //     const bus = {
+    //       firstName: 'xanda',
+    //       lastName: 'cage',
+    //       email: 'cage@yahoo.com',
+    //       tenor: '3 month',
+    //       amount: '55,000'
+    //     };
+    //     request.post('/api/v1/loans')
+    //       .send(bus)
+    //       .expect(201)
+    //       .end((err, res) => {
+    //         console.log(res.body, '===================');
+    //         done();
+    //       });
+    //   });
+    // });
+    describe('All test case loan Application', () => {
+      it('Should create a new bus and return a `201`', (done) => {
+        const bus = {
+          number_plate : ' ',
+          manufacturer: ' ',
+          model: ' ',
+          year: ' ',
+          capacity: ' '
+        };
+        // number_plate, manufacturer, model, year, capacity
+        request.post('/api/v1/bus')
+          .send(bus)
+          .expect(401)
+          .end((err, res) => {
+            console.log(res.body);
+            done();
           });
-  //     });
+      });
     });
   });
 });
+  });
+});
+});
+
