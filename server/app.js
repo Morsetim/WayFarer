b9531ed58d75cf8e26ca90e9caa3179a0b324508
Mors Etim <maur@gmail.com>
2021-06-10 16:49:09 +0100

    ggd

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import apiRoute from './route/router';
 
 
 const app = express();
-const port = parseInt((process.env.PORT), 10) || 5000
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
